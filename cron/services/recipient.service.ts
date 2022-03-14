feb2878b1963bb557281d57c002d468fe363e069
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2022-03-14 15:51:22 -0700

    set language to English

--- cron/services/recipient.service.ts
+++ cron/services/recipient.service.ts
@@ -5,7 +5,8 @@ export const getRecipients = async () => {
     let recipients: any
      try{
          await db.query(
-             `SELECT * FROM api.recipients WHERE "emailSent" IS FALSE AND "emailError" IS FALSE AND "surveyCompleted" IS FALSE`
+             `SELECT * FROM api.recipients WHERE "emailSent" IS FALSE AND "emailError" IS FALSE AND "surveyCompleted" IS FALSE AND "language" = $1`,
+             ["EN"]
            )
          .then((resp: any) => {
              recipients = { count: resp.rowCount, recipients: resp.rows.map((c: any) => {
